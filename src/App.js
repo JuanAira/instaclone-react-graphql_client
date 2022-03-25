d9b75ea5d07737570b3f083976c218bc62e805ab
JuanAira <juanaira_01@hotmail.com>
2022-03-25 18:03:36 -0300

    Add config initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  );
+}
