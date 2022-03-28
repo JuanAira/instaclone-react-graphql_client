25050d9523313d3287aedb461af6e5ee4f417126
JuanAira <juanaira_01@hotmail.com>
2022-03-28 13:06:33 -0300

    Add utils

---
+++ src/utils/token.js
@@ -0,0 +1,10 @@
+import jwtDecode from 'jwt-decode';
+import { TOKEN } from './constants';
+
+export const setToken = ({ token }) => { localStorage.setItem(TOKEN, token); };
+
+export const getToken = () => localStorage.getItem(TOKEN);
+
+export const decodeToken = ({ token }) => jwtDecode(token);
+
+export const removeToken = () => { localStorage.removeItem(TOKEN); };
