385a71942e60c6789d7b20888fd4e05ae7853b81
JuanAira <juanaira_01@hotmail.com>
2022-03-29 19:10:21 -0300

    Update page error

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react';
 
+import imageNotFound from '../../assets/page-not-found.png';
 import './sass/styles.scss';
+
+const nameSpace = 'page-not-found';
 
 export default function Error() {
   return (
-    <div>Estamos en error</div>
+    <div className={nameSpace}>
+      <Image className={`${nameSpace}--image`} src={imageNotFound} alt="page not found" />
+    </div>
   );
 }
