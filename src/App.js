fb9e24d58d5dd0787b1c3d66af0fe67943aa5a51
JuanAira <juanaira_01@hotmail.com>
2022-03-29 19:09:57 -0300

    Add index

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { ToastContainer } from 'react-toastify';
 import client from './config/apollo';
-import Auth from './pages/Auth';
+import { AppRouter } from './routes/AppRouter';
+import AuthContext from './context/AuthContext';
+import { getToken, decodeToken, removeToken } from './utils/token';
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(undefined);
+  const [auth, setAuth] = useState(undefined);
+
+  useEffect(() => {
+    const token = getToken();
+    if (!token) {
+      setAuth(null);
+    } else {
+      setAuth(decodeToken({ token }));
+    }
+  }, []);
+
+  const logout = () => {
+    removeToken();
+    setAuth(null);
+  };
+
+  const setUser = ({ user }) => {
+    setAuth(user);
+  };
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      logout,
+      setUser,
+    }),
+    [auth],
+  );
+
+  if (auth === undefined) return null;
 
   return (
     <ApolloProvider client={client}>
-      {!isAuthenticated ? <Auth /> : <div>Loading...</div>}
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+      <AuthContext.Provider value={authData}>
+        <AppRouter />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </AuthContext.Provider>
     </ApolloProvider>
   );
 }
